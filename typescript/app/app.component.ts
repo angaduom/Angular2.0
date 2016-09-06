98860af7861ee8d0d85949831c7386ff58902a83
angaduom <angaduom@gmail.com>
2016-09-05 21:35:29 -0400

    First commit, next adding golang

---
+++ typescript/app/app.component.ts
@@ -0,0 +1,8 @@
+import {Component} from 'angular2/core'
+
+@Component({
+    selector: 'my-app',
+    template: `<h1>My First Angular 2 App </h1>
+    			<p>Now I have to connect using socket and then add a GOLang server</p>`
+})
+export class AppComponent { }
