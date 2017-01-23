b92a6b2ad01e5d96a4d363796f181c83667b8409
angaduom <angaduom@gmail.com>
2017-01-23 12:35:53 -0600

    Deleted the previous changes so that I can learn from scratch again

--- typescript/app/app.component.ts
+++
@@ -1,8 +0,0 @@
-import {Component} from 'angular2/core'
-
-@Component({
-    selector: 'my-app',
-    template: `<h1>My First Angular 2 App </h1>
-    			<p>Now I have to connect using socket and then add a GOLang server</p>`
-})
-export class AppComponent { }
